cab61f70fda5fc51f9b6db810697a1296a3b9d4d
vbskycn <34542650+vbskycn@users.noreply.github.com>
2024-09-11 16:17:38 +0800

    添加统计

---
+++ docs/js/analytics.js
@@ -0,0 +1,2 @@
+// 51.la统计代码
+!function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"23VI72XcGSs4VQpA",ck:"23VI72XcGSs4VQpA"});
