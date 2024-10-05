92da8b04237c0fb6f268da1ee93998dbf91c8eb0
vbskycn <34542650+vbskycn@users.noreply.github.com>
2024-10-05 20:51:06 +0800

    新增google ads

---
+++ docs/js/google_ads.js
@@ -0,0 +1,7 @@
+(function() {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6941806026814791';
+  script.crossOrigin = 'anonymous';
+  document.head.appendChild(script);
+})();
